5735e61f953c06ffca2c0c954073d26f8201feab
zhanglei1949 <zhanglei1949@sjtu.edu.cn>
2017-07-20 09:27:20 +0800

    enable ceph version 11.2.0(kraken) in /deploy/mod/deploy.py

--- deploy/mod/deploy.py
+++ deploy/mod/deploy.py
@@ -165,7 +165,8 @@ class Deploy(object):
                        'giant': '0.87',
                        'hammer': '0.94',
                        'infernalis': '9.2',
-		       'jewel': '10.2'}
+		       'jewel': '10.2',
+                       'kraken' : '11.2.0'}
         for node, version_code in installed.items():
             if version == "":
                 for release_name, short_version in version_map.items():
